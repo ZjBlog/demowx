6f78dd57416e50925849cf2cf68847bacb13f89d
zhangjunTracy <13552960783@163.com>
2017-09-13 17:39:10 +0800

    demo

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -3,7 +3,17 @@ Page({
   data: {
     title: '',
     toView: 'red',
-    scrollTop: 100
+    scrollTop: 0,
+    imgUrls:[
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    circular: false
   },
   onLoad: function () {
     this.setData({
@@ -11,12 +21,15 @@ Page({
     })
   },
   upper: function (e) {
+    console.info('到顶了')
     console.log(e)
   },
   lower: function (e) {
+    console.info('到底了')
     console.log(e)
   },
   scroll: function (e) {
+    console.info('开始滚了')
     console.log(e)
   },
   tap: function (e) {
@@ -33,5 +46,23 @@ Page({
     this.setData({
       scrollTop: this.data.scrollTop + 10
     })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  bchange (e) {
+    console.info(e)
   }
 })
