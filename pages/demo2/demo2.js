941433b7f8a41d49355a87d9b653d9b3792b530d
zhangjunTracy <13552960783@163.com>
2017-09-15 14:18:33 +0800

    demo

--- pages/demo2/demo2.js
+++ pages/demo2/demo2.js
@@ -1,6 +1,8 @@
 var types = ['default', 'primary', 'warn']
 Page({
   data: {
+    focus: false,
+    inputValue: '',
     title: '',
     defaultSize: 'default',
     primarySize: 'default',
@@ -15,7 +17,24 @@ Page({
       { name: 'JPN', value: '日本' },
       { name: 'ENG', value: '英国' },
       { name: 'TUR', value: '法国' }
-    ]
+    ],
+    checkboxItems: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本', checked: 'true' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ],
+    radioItems: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ],
+    hidden: false
   },
   //生命周期函数--监听页面加载
   onLoad: function () {
@@ -55,5 +74,65 @@ Page({
   },
   checkboxChange: function (e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
+  },
+  bindButtonTap: function () {
+    this.setData({
+      focus: true
+    })
+  },
+  bindKeyInput: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  bindReplaceInput: function (e) {
+    var value = e.detail.value
+    var pos = e.detail.cursor
+    if (pos != -1) {
+      //光标在中间
+      var left = e.detail.value.slice(0, pos)
+      //计算光标的位置
+      pos = left.replace(/11/g, '2').length
+    }
+
+    //直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
+    return {
+      value: value.replace(/11/g, '2'),
+      cursor: pos
+    }
+
+    //或者直接返回字符串,光标在最后边
+    //return value.replace(/11/g,'2'),
+  },
+  checkboxChange: function (e) {
+    console.info(e)
+    var checked = e.detail.value
+    var changed = {}
+    for (var i = 0; i < this.data.checkboxItems.length; i++) {
+      if (checked.indexOf(this.data.checkboxItems[i].name) !== -1) {
+        changed['checkboxItems[' + i + '].checked'] = true
+      } else {
+        changed['checkboxItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
+  },
+  radioChange: function (e) {
+    var checked = e.detail.value
+    var changed = {}
+    for (var i = 0; i < this.data.radioItems.length; i++) {
+      if (checked.indexOf(this.data.radioItems[i].name) !== -1) {
+        changed['radioItems[' + i + '].checked'] = true
+      } else {
+        changed['radioItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
   }
 })
---
+++ pages/demo4/demo4.js
@@ -0,0 +1,47 @@
+Page({
+  data: {
+    title: '表单剩余组件',
+    items: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ],
+    height: 20,
+    focus: false
+  },
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+  slider1change (e) {
+    console.log('发生 change 事件，携带值为', e.detail.value)
+  },
+    slider2change (e) {
+    console.log('发生 change 事件，携带值为', e.detail.value)
+  },
+  slider3change (e) {
+    console.log('发生 change 事件，携带值为', e.detail.value)
+  },
+  slider4change (e) {
+      console.log('发生 change 事件，携带值为', e.detail.value)
+  },
+  switch1Change: function (e) {
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  },
+  switch2Change: function (e) {
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  },
+  bindButtonTap: function () {
+    this.setData({
+      focus: true
+    })
+  },
+  bindTextAreaBlur: function (e) {
+    console.log(e.detail.value)
+  },
+  bindFormSubmit: function (e) {
+    console.log(e.detail.value.textarea)
+  }
+})
