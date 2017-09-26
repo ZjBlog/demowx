baa35d6394ac606680806a5ccf64a58ab003a485
zhangjunTracy <13552960783@163.com>
2017-09-26 22:17:53 +0800

    ana

--- pages/index1/index1.js
+++ pages/index1/index1.js
@@ -6,24 +6,25 @@ Page({
     movieList: [
       { url: 'https://api.douban.com/v2/movie/in_theaters' },
       { url: 'https://api.douban.com/v2/movie/coming_soon' },
-      { url: 'https://api.douban.com/v2/movie/top250' },
-      { url:'https://api.douban.com/v2/movie/us_box'}
+      { url: 'https://api.douban.com/v2/movie/top250' }
+      //有次数限制 先去掉 其它完成之后加上 加缓存
+     // { url:'https://api.douban.com/v2/movie/us_box'}
     ],
   },
   //生命周期函数--监听页面加载
   onLoad: function () {
-    let allPromises = this.data.movieList.map(m =>{
-      return requsetForGet(m.url,{count: 10}).then((res) =>{
-          m.title = res.data.title
-          m.subjects = res.data.subjects
-          return m
-      })
-    })
-    Promise.all(allPromises).then((res)=>{
-      this.setData({
-        movieList: res
-      })
-      console.info(res)
-    })
+    // let allPromises = this.data.movieList.map(m =>{
+    //   return requsetForGet(m.url,{count: 10}).then((res) =>{
+    //       m.title = res.data.title
+    //       m.subjects = res.data.subjects
+    //       return m
+    //   })
+    // })
+    // Promise.all(allPromises).then((res)=>{
+    //   this.setData({
+    //     movieList: res
+    //   })
+    //   console.info(res)
+    // })
   }
 })
---
+++ utils/ana.js
@@ -0,0 +1,14 @@
+/**
+ * 经典台词https://wenku.baidu.com/view/c9085b0933d4b14e8424685e.html
+ */
+
+let anas = []
+anas.push('不要相信漂亮女人，尤其是不穿衣服的女人。——《偷天陷阱》')
+anas.push('做人如果没有梦想，那和咸鱼有什么区别？——《少林足球》')
+anas.push('我现在才知道，他能够开开心心在外面走来走去的，是因为他知道始终有着一个地方等着他！——《春光乍泄》')
+function getAna () {
+  
+}
+module.exports = {
+  getAna
+}
