2614486f85e5ae1ecd1e47bb81243769c95ce818
zhangjunTracy <13552960783@163.com>
2017-09-27 19:23:48 +0800

    lis

--- pages/index1/index1.js
+++ pages/index1/index1.js
@@ -14,18 +14,28 @@ Page({
   },
   //生命周期函数--监听页面加载
   onLoad: function () {
-  //   let allPromises = this.data.movieList.map(m =>{
-  //     return requsetForGet(m.url,{count: 10}).then((res) =>{
-  //         m.title = res.data.title
-  //         m.subjects = res.data.subjects
-  //         return m
-  //     })
-  //   })
-  //   Promise.all(allPromises).then((res)=>{
-  //     this.setData({
-  //       movieList: res
-  //     })
-  //     console.info(res)
-  //   })
+    // let allPromises = this.data.movieList.map(m =>{
+    //   return requsetForGet(m.url,{count: 10}).then((res) =>{
+    //       m.title = res.data.title
+    //       m.subjects = res.data.subjects
+    //       return m
+    //   })
+    // })
+    // Promise.all(allPromises).then((res)=>{
+    //   this.setData({
+    //     movieList: res
+    //   })
+    //   console.info(res)
+    // })
+  },
+  more(event) {
+    console.info(event)
+    let url = event.currentTarget.dataset.more
+    let index = url.lastIndexOf('\/')
+    url = url.substring(index + 1, url.length)
+    console.info(url)
+    wx.navigateTo({
+      url: '/pages/list/list?type=' + url
+    })
   }
 })
